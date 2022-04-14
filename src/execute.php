70b6728d370631a219d7310d53730238a12c13e1
Julien Boudry <julien.boudry@gmail.com>
2022-04-14 17:32:03 +0200

    Add Summary & Pairwise, improve KemenyYoung ....

--- src/execute.php
+++ src/execute.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace CondorcetPHP\TidemanCollection;
 
+use CondorcetPHP\Condorcet\Algo\Methods\KemenyYoung\KemenyYoung;
 use CondorcetPHP\Condorcet\Condorcet;
 use CondorcetPHP\Condorcet\Election;
 use CondorcetPHP\Condorcet\Throwable\CondorcetPublicApiException;
@@ -15,20 +16,33 @@ use CondorcetPHP\Condorcet\Tools\TidemanDataCollection;
 require_once __DIR__.'/../vendor/autoload.php';
 
 // Memory Limit
-ini_set('memory_limit', '4096M');
+ini_set('memory_limit', '8192M');
+
+
+    $isTest = false;
 
     // Scan collection directory
 
     $tideman_collection_list = [];
 
     $dir = __DIR__.'/../Input_Tideman_Election_Collection/';
-    foreach (\array_diff(\scandir($dir), ['.', '..']) as $fileName) :
+
+    $scandir = \array_diff(\scandir($dir), ['.', '..']);
+    natsort($scandir);
+
+    $i = 0;
+    foreach ($scandir as $fileName) :
         $tideman_collection_list[\str_replace('.HIL', '', $fileName)] = $dir.$fileName;
+        if ($isTest && ++$i > 4) : break; endif; # Uncomment for quick dev tests
     endforeach;
 
     natsort($tideman_collection_list);
 
     // Compute
+
+    $methods = Condorcet::getAuthMethods();
+    natsort($methods);
+    !$isTest && KemenyYoung::$MaxCandidates = 9;
 
     foreach ($tideman_collection_list as $name => $path) :
         echo 'Execute: '.$name."\n";
@@ -39,22 +53,32 @@ ini_set('memory_limit', '4096M');
         $results[$name] = [];
 
         // Implicit Ranking
-        computeResults($election, 'implicitRankingEvaluationOfVotes', $results[$name], $name);
+        computeResults($election, 'implicitRankingEvaluationOfVotes', $results[$name], $name, $methods);
 
         // Explicit Ranking OFF
         $election->setImplicitRanking(false);
-        computeResults($election, 'explicitRankingEvaluationOfVotes', $results[$name], $name);
+        computeResults($election, 'explicitRankingEvaluationOfVotes', $results[$name], $name, $methods);
+
+        $results[$name]['condorcetFormatVotes'] = $election->getVotesListAsString();
     endforeach;
 
     unset($collection, $election);
 
 
     // Export Results
+
     $create_dir = function (string $dir):void {if (!is_dir($dir)) : mkdir($dir); endif;};
 
+    # Export for each election
     foreach ($results as $name => $electionResults) :
         foreach ($electionResults as $methodName => $methodResult) :
+            if ($methodName === 'condorcetFormatVotes') : continue; endif;
+            if ($methodName === 'Pairwise') : continue; endif;
+
+
             foreach ($methodResult as $mode => $oneResult) :
+                if (!$oneResult['active']) : continue; endif;
+
                 echo 'Write Result: '.$name.' - '.$mode.' - '.$methodName."\n";
 
                 $json = json_encode([
@@ -73,30 +97,93 @@ ini_set('memory_limit', '4096M');
                 $path = "$dir/$name-$mode-$methodName.json";
 
                 file_put_contents($path, $json);
-
-                // Condorcet Format
-                file_put_contents("$base_dir/$name-aggregated_votes.cvotes", $oneResult['votes_aggregated_condorcetFormat']);
             endforeach;
+        endforeach;
+
+        // Condorcet Format
+        file_put_contents("$base_dir/$name-aggregated_votes.cvotes", $electionResults['condorcetFormatVotes']);
+    endforeach;
+
+    # Export Pairwise
+    foreach ($results as $name => $electionResults) :
+        foreach ($electionResults as $methodName => $methodResult) :
+            if ($methodName === 'Pairwise') :
+                foreach ($methodResult as $mode => $pairwise) :
+                    $path = __DIR__."/../Results_Output/$name/$mode/$name-$mode-Pairwise.json";
+
+                    echo "Write Pairwise: $name - $mode\n";
+                    file_put_contents($path, json_encode($pairwise, \JSON_PRETTY_PRINT));
+                endforeach;
+            endif;
         endforeach;
     endforeach;
 
-
+    # Make summary
+    makeSummary($methods, $results, 'implicitRankingEvaluationOfVotes');
+    makeSummary($methods, $results, 'explicitRankingEvaluationOfVotes');
 
     // Functions
 
-    function computeResults (Election $election, string $index, array &$results, string $name)
+    function computeResults (Election $election, string $index, array &$results, string $name, array $methods)
     {
-        foreach (Condorcet::getAuthMethods() as $method) :
+        foreach ($methods as $method) :
             try {
                 echo 'Compute method: '.$name.' - '.$index.' - '.$method."\n";
                 $results[$method][$index] = [
-                    'ranking' => $election->getResult($method)->getResultAsString(),
-                    'stats'   => $election->getResult($method)->getStats(),
-                    'number_of_seats'       => $election->getNumberOfSeats(),
-                    'votes_aggregated_condorcetFormat' => $election->getVotesListAsString()
+                    'active'            => true,
+                    'ranking'           => $election->getResult($method)->getResultAsString(),
+                    'stats'             => $election->getResult($method)->getStats(),
+                    'number_of_seats'    => $election->getNumberOfSeats(),
                 ];
             } catch (CondorcetPublicApiException $e) {
                 echo $e->getMessage();
+
+                $results[$method][$index] = [
+                    'active'            => false,
+                    'ranking'           => null,
+                    'stats'             => null,
+                    'number_of_seats'   => $election->getNumberOfSeats(),
+                ];
             }
         endforeach;
+
+        $results['Pairwise'][$index] = $election->getExplicitPairwise();
+    }
+
+    function makeSummary (array $methods, array $results, string $mode): void
+    {
+        $md = '| --- | Pairwise |';
+
+        foreach ($methods as $methodName) :
+            $md .= " $methodName |";
+        endforeach;
+
+        $md .= "\n| --- |";
+
+        for($i=0 ; $i < (count($methods) + 1) ; $i++) :
+            $md .= ' --- |';
+        endfor;
+        $md .= "\n";
+
+        foreach ($results as $name => $electionResults) :
+
+            $md .= "| $name |";
+
+            $md .= " _[Pairwise](Results_Output/$name/$mode/$name-$mode-Pairwise.json)_".' |';
+
+            foreach ($electionResults as $methodName => $methodResult) :
+                if ($methodName === 'condorcetFormatVotes') : continue; endif;
+                if ($methodName === 'Pairwise') : continue; endif;
+
+                foreach ($methodResult as $voteMode => $oneResult) :
+                    if ($voteMode === $mode) :
+                        $md .= ' '.$oneResult['ranking'].' |';
+                    endif;
+                endforeach;
+            endforeach;
+
+            $md .= "\n";
+        endforeach;
+
+        file_put_contents(__DIR__."/../$mode-summary.md", $md);
     }
